f1686c0f44248de4394ce305e29b8ab8782fd1f8
Karen Aitana <karenaitana21@gmail.com>
2023-11-19 10:34:09 -0500

    Patient Model

---
+++ CiudadanosSanos/CiudadanosSanos/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CiudadanosSanos.Models
+{
+	public class Patient
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "The Name is required")]
+		public string Name { get; set; }
+		public ICollection<MedicalAttention> MedicalAttentions { get; set; }
+	}
+}
