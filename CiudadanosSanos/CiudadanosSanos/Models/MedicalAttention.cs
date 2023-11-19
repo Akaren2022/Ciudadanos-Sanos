09bb8ed12915725b6c0f265d35a6cba897b68b2a
Karen Aitana <karenaitana21@gmail.com>
2023-11-19 10:33:11 -0500

    Medical Attention Model

---
+++ CiudadanosSanos/CiudadanosSanos/Models/MedicalAttention.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace CiudadanosSanos.Models
+{
+	public class MedicalAttention
+	{
+		public int Id { get; set; }
+
+		[DataType(DataType.Date)]
+		[Display(Name = "Creation Date")]
+		public DateTime? Fecha { get; set; }
+
+		[Display(Name = "Medic Name")]
+		[Required(ErrorMessage = "The Medic Name is required")]
+		public string MedicName { get; set; }
+
+		public int PatientId { get; set; }
+		public Patient Cliente { get; set; }
+	}
+}
