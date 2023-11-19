c76fd1ebb4dbc49b92be0b7e0a6468900d1d5684
Karen Aitana <karenaitana21@gmail.com>
2023-11-19 12:52:28 -0500

    Medical Attention Index

---
+++ CiudadanosSanos/CiudadanosSanos/Pages/MedicalAttention/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using CiudadanosSanos.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CiudadanosSanos.Pages.MedicalAttention
+{
+    public class IndexModel : PageModel
+    {
+		private readonly CiudadanosSanosContext _context;
+		public IndexModel(CiudadanosSanosContext context)
+		{
+			_context = context;
+		}
+		public IEnumerable<Models.MedicalAttention> MedicalAttentions { get; set; }
+		public async Task OnGet()
+		{
+			MedicalAttentions = await _context.MedicalAttentions.Include(c => c.Patient).ToListAsync();
+		}
+	}
+}
