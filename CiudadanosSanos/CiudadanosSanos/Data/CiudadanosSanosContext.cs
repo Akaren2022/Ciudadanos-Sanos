3d15da555783910d6680738cb5d0d133d81dadb1
Karen Aitana <karenaitana21@gmail.com>
2023-11-19 11:05:31 -0500

    DbContext

---
+++ CiudadanosSanos/CiudadanosSanos/Data/CiudadanosSanosContext.cs
@@ -0,0 +1,22 @@
+﻿using CiudadanosSanos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CiudadanosSanos.Data
+{
+	public class CiudadanosSanosContext: DbContext
+	{
+		public CiudadanosSanosContext(DbContextOptions options) : base(options) { 
+		
+		}
+		public DbSet<User> Users { get; set; }
+		public DbSet<Patient> Patients { get; set; }
+		public DbSet<MedicalAttention> MedicalAttentions { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(
+				"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CiudadanosSanosDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
+				);
+		}
+	}
+}
